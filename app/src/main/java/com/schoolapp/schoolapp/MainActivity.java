e79cdef4c9390a0735e99d524d63c805091c783e
Lohni <andi.lohninger@hotmail.com>
2018-10-19 16:40:56 +0200

    Main Layout

--- app/src/main/java/com/schoolapp/schoolapp/MainActivity.java
+++ app/src/main/java/com/schoolapp/schoolapp/MainActivity.java
@@ -4,24 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+
 import com.ebanx.swipebtn.OnStateChangeListener;
 import com.ebanx.swipebtn.SwipeButton;
 
+
 public class MainActivity extends AppCompatActivity {
 
-        SwipeButton swipeButton = (SwipeButton)findViewById(R.id.swipe_btn);
+        SwipeButton musicbtn;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.mainmenu);
 
-            swipeButton.setOnStateChangeListener(new OnStateChangeListener() {
+            musicbtn = findViewById(R.id.musicbtn);
+            musicbtn.setOnStateChangeListener(new OnStateChangeListener() {
                 @Override
                 public void onStateChange(boolean active) {
-                    
+                    Toast.makeText(MainActivity.this, "Test"+ active, Toast.LENGTH_SHORT).show();
                 }
             });
+
 
         }
 
