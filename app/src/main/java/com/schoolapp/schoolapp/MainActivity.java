defcbda1dc130d2af9f146bac2a65135f1af15e2
Lohni <andi.lohninger@hotmail.com>
2018-10-19 10:54:12 +0200

    SwipeButton

--- app/src/main/java/com/schoolapp/schoolapp/MainActivity.java
+++ app/src/main/java/com/schoolapp/schoolapp/MainActivity.java
@@ -2,21 +2,28 @@ package com.schoolapp.schoolapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-    public class MainActivity extends AppCompatActivity {
+import com.ebanx.swipebtn.OnStateChangeListener;
+import com.ebanx.swipebtn.SwipeButton;
+
+public class MainActivity extends AppCompatActivity {
+
+        SwipeButton swipeButton = (SwipeButton)findViewById(R.id.swipe_btn);
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.mainmenu);
-            thisIsATest();
+
+            swipeButton.setOnStateChangeListener(new OnStateChangeListener() {
+                @Override
+                public void onStateChange(boolean active) {
+                    
+                }
+            });
+
         }
-
-        private void thisIsATest() {
-
-            System.out.println("Dei Muata sei Gsicht");
-        }
-
 
 
     }
