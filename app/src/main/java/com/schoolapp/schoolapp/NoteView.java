371a31bf17bf935d2b07d369f68252ebaff821b3
Lohni <andi.lohninger@hotmail.com>
2018-10-24 12:18:04 +0200

    Notes

--- app/src/main/java/com/schoolapp/schoolapp/NoteView.java
+++ app/src/main/java/com/schoolapp/schoolapp/NoteView.java
@@ -1,14 +1,18 @@
 package com.schoolapp.schoolapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,8 +22,6 @@ import java.io.OutputStreamWriter;
 public class NoteView extends AppCompatActivity {
 
     private static final String TAG = "NoteView";
-
-    FloatingActionButton savenote, deletenote;
     EditText notename, notetext;
 
     @Override
@@ -27,39 +29,62 @@ public class NoteView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note_view);
 
-        savenote = findViewById(R.id.savenote);
-        deletenote = findViewById(R.id.deletenote);
+
+
+        Button savenote = findViewById(R.id.savenote);
+        Button deletenote = findViewById(R.id.deletenote);
         notename = findViewById(R.id.notename);
         notetext = findViewById(R.id.notetext);
+        Button revertbtn = findViewById(R.id.revertbtn);
 
-        final String name = notename.getText().toString();
-        final String text = notetext.getText().toString();
+
+
+        if(!getIntent().getStringExtra("Name").isEmpty()){
+            final String oldname = getIntent().getStringExtra("Name");
+            notetext.setText(readNote(oldname));
+            notename.setText(oldname);
+        }
 
         savenote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String name = notename.getText().toString();
+                final String text = notetext.getText().toString();
                 saveNoteText(name, text);
             }
         });
         deletenote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String name = notename.getText().toString();
                 deleteNote(name);
+            }
+        });
+        revertbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
 
     private void deleteNote(String name) {
+        File f = new File(name);
+        boolean delete = f.delete();
+
+        if(delete){Toast.makeText(this, "Successfully deleted", Toast.LENGTH_SHORT).show(); finish();}
+        else Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
     }
 
     private void saveNoteText(String name, String text) {
-
         if (name.isEmpty()) Toast.makeText(this, "Field Name is empty", Toast.LENGTH_SHORT).show();
         else {
-            try (FileOutputStream fos = openFileOutput(name, MODE_APPEND);
+            try (FileOutputStream fos = openFileOutput(name, MODE_PRIVATE);
 
                  OutputStreamWriter osw = new OutputStreamWriter(fos)) {
                 osw.write(name + System.lineSeparator() + text);
+                fos.close();
+                Toast.makeText(NoteView.this, "Data successfully saved!", Toast.LENGTH_SHORT).show();
             } catch (IOException t) {
                 Log.d(TAG, "saveNoteText: ", t);
             }
@@ -68,7 +93,7 @@ public class NoteView extends AppCompatActivity {
 
     private String readNote(String name) {
         StringBuilder stringBuilder = new StringBuilder();
-        try (FileInputStream fis = openFileInput(name);
+        try (FileInputStream fis = openFileInput("" + name);
              InputStreamReader isr = new InputStreamReader(fis);
              BufferedReader br = new BufferedReader(isr)) {
             String s;
--- app/src/main/java/com/schoolapp/schoolapp/Notes.java
+++ app/src/main/java/com/schoolapp/schoolapp/Notes.java
@@ -1,20 +1,28 @@
 package com.schoolapp.schoolapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
 
 public class Notes extends AppCompatActivity {
 
-    ListView listView;
-    FloatingActionButton newitem;
-    FloatingActionButton backbtn;
     ListAdapter listAdapter;
+    ArrayList<String> listdata;
+    ListView noteView;
+    Button newitem;
+    Button backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +30,15 @@ public class Notes extends AppCompatActivity {
         setContentView(R.layout.activity_notes);
 
         getListcontent();
-        listView = findViewById(R.id.notelist);
-        newitem = findViewById(R.id.newitem);
+        newitem = findViewById(R.id.additem);
         backbtn = findViewById(R.id.backbtn);
 
         newitem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent i = new Intent(Notes.this, NoteView.class);
+                i.putExtra("Name", "");
+                startActivity(i);
             }
         });
 
@@ -39,10 +48,30 @@ public class Notes extends AppCompatActivity {
                 finish();
             }
         });
+        noteView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String currentItem = listAdapter.getItem(position).toString();
+                Intent i = new Intent(Notes.this, NoteView.class);
+                i.putExtra("Name", currentItem);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getListcontent();
     }
 
     private void getListcontent() {
-        File f;
 
+        String[] f = fileList();
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, f);
+        noteView = findViewById(R.id.notelist);
+        noteView.setAdapter(listAdapter);
     }
+
+
 }
